0afaf36039ba715226df5abbe2ecc3e46dd26168
Sebastian Prestele <sebastian.prestele@gmail.com>
2022-02-18 22:36:51 +0100

    Added loading spinner on shop for wait during  API call

--- src/Components/Shop/Shop.tsx
+++ src/Components/Shop/Shop.tsx
@@ -1,15 +1,30 @@
+import { FaSpinner } from "react-icons/fa";
 import Footer from "Components/Footer/Footer";
 import Navigation from "Components/Navigation/Navigation";
-import ProductCard from "./ProductCard";
 import Sorting from "Components/Sorting/Sorting";
+import ProductCard from "./ProductCard";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "Redux/Reducers/rootReducer";
 
 const Shop = () => {
+  const { isLoading } = useSelector(
+    (state: RootState) => state.productsReducer
+  );
+
+  console.log(isLoading);
+
   return (
     <div className="page-wrapper">
       <Navigation />
       <Sorting />
       <div className="shop-page">
-        <h1>FinArts Shop</h1>
+        {isLoading ? (
+          <h1>
+            <FaSpinner className="spinner" />
+          </h1>
+        ) : (
+          <h1>FinArts Shop</h1>
+        )}
         <div className="shop-product-container">
           <ProductCard />
         </div>
